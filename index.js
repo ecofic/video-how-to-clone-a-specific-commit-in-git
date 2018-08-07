5d463f6752aaff07b4a012da2c6e00abb88253e7
Chad Campbell <chad.campbell@gmail.com>
2018-08-07 19:19:24 -0400

    Changed text color to Red.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let text = 'Hello, World!';
 
 console.log('\n');
-console.log('\x1b[32m', text);         // Print text in green
+console.log('\x1b[31m', text);         // Print text in red
 console.log('\x1b[37m' , '\n');        // Reset the console color
